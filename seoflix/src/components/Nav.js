1a2d404040d4cfc746d7e51b885f8d5671ff273a
seodoha <doha.seo89@gmail.com>
2022-10-24 11:10:03 +0900

    헤더 작업중

--- seoflix/src/components/Nav.js
+++ seoflix/src/components/Nav.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import styles from './Nav.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
     return (
@@ -12,9 +14,34 @@ function Nav() {
                         <strong>SEOFLIX</strong>
                     </Link>
                 </h1>
-                <ul>
-                    <li></li>
+                <ul className={styles.nav__list}>
+                    <li>
+                        <Link to="/">High Rating</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Romance</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Horror</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Animation</Link>
+                    </li>
                 </ul>
+                <div className={styles.head__search}>
+                    <input
+                        placeholder='search movie'
+                    />
+                    <FontAwesomeIcon
+                        icon={faMagnifyingGlass}
+                        style={{
+                            width: "2.5rem",
+                            height: "2.5rem",
+                            color: "#fff",
+                            cursor: "pointer"
+                        }}
+                    />
+                </div>
             </nav>
         </div>
     );
